7efa6831208c9b152910865b5bc04e89ffb4aa30
Carolina Alvim <caro.alvim@gmail.com>
2022-04-04 20:22:22 -0300

    task2

---
+++ src/components/Cronometro/cronometro.tsx
@@ -0,0 +1,13 @@
+function Cronometro(){
+    return(
+        <div>
+            <p>Escolha um card e inicie o cronometro</p>
+            <div>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Cronometro;
